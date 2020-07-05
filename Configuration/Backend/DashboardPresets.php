940d869166a507a514ff4180f232504751ea2cdf
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-07-05 13:17:55 +0200

    [FEATURE] Add dashboard preset

---
+++ Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the "matomo_widgets" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+return [
+    'matomo' => [
+        'title' => 'LLL:EXT:matomo_widgets/Resources/Private/Language/Dashboard.xlf:preset.title',
+        'description' => 'LLL:EXT:matomo_widgets/Resources/Private/Language/Dashboard.xlf:preset.description',
+        'iconIdentifier' => 'content-dashboard',
+        'defaultWidgets' => [
+            'matomo_widgets.visitsSummary.visitsPerDay',
+            'matomo_widgets.visitsSummary.actionsPerDay',
+            'matomo_widgets.visitsSummary.visitsPerMonth',
+            'matomo_widgets.visitsSummary.actionsPerMonth',
+            'matomo_widgets.visitsSummary.bounceRate',
+        ],
+        'showInWizard' => true
+    ],
+];
