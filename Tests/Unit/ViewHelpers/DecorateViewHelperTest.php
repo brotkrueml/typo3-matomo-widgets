407e2881d9be3b067821ab3a35c8a098dc51e1e9
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-07-16 18:09:25 +0200

    [TASK] Add tests for view helper

---
+++ Tests/Unit/ViewHelpers/DecorateViewHelperTest.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the "matomo_widgets" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Brotkrueml\MatomoWidgets\Tests\Unit\ViewHelpers;
+
+use Brotkrueml\MatomoWidgets\ViewHelpers\DecorateViewHelper;
+use Brotkrueml\MatomoWidgets\Widgets\Decorator\DecoratorInterface;
+use PHPUnit\Framework\MockObject\Stub;
+use PHPUnit\Framework\TestCase;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
+
+class DecorateViewHelperTest extends TestCase
+{
+    /**
+     * @var Stub|RenderingContextInterface
+     */
+    private $renderingContextStub;
+
+    protected function setUp(): void
+    {
+        $this->renderingContextStub = $this->createStub(RenderingContextInterface::class);
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProviderForRenderStatic
+     * @param array $arguments
+     * @param string $expected
+     */
+    public function renderStatic(array $arguments, string $expected): void
+    {
+        $actual = DecorateViewHelper::renderStatic(
+            $arguments,
+            function () {
+            },
+            $this->renderingContextStub
+        );
+
+        self::assertSame($expected, $actual);
+    }
+
+    public function dataProviderForRenderStatic(): \Generator
+    {
+        yield 'Given string value is decorated' => [
+            'arguments' => [
+                'decorator' => $this->getDecoratorStub(),
+                'value' => 'bar',
+            ],
+            'expected' => '---bar---',
+        ];
+
+        yield 'Given int value is decorated' => [
+            'arguments' => [
+                'decorator' => $this->getDecoratorStub(),
+                'value' => 42,
+            ],
+            'expected' => '---42---',
+        ];
+    }
+
+    protected function getDecoratorStub()
+    {
+        return new class() implements DecoratorInterface {
+            public function decorate(string $value): string
+            {
+                return \sprintf('---%s---', $value);
+            }
+
+            public function isHtmlOutput(): bool
+            {
+                return false;
+            }
+        };
+    }
+
+    /**
+     * @test
+     */
+    public function renderStaticThrowsExceptionWhenDecoratorImplementsNotDecoratorInterface(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(1594828163);
+        $this->expectExceptionMessage('The decorator "stdClass" is not an instance of "Brotkrueml\\MatomoWidgets\\Widgets\\Decorator\\DecoratorInterface"');
+
+        $arguments = [
+            'decorator' => new \stdClass(),
+            'value' => 'foo',
+        ];
+
+        DecorateViewHelper::renderStatic(
+            $arguments,
+            function () {
+            },
+            $this->renderingContextStub
+        );
+    }
+}
