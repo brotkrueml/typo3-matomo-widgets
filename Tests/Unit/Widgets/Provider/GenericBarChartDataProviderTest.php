f90589fdcb334237a44ba986074705ae09a63ffd
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-07-16 18:29:05 +0200

    [TASK] Add tests for data provider

---
+++ Tests/Unit/Widgets/Provider/GenericBarChartDataProviderTest.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the "matomo_widgets" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Brotkrueml\MatomoWidgets\Tests\Unit\Widgets\Provider;
+
+use Brotkrueml\MatomoWidgets\Domain\Repository\RepositoryInterface;
+use Brotkrueml\MatomoWidgets\Parameter\ParameterBag;
+use Brotkrueml\MatomoWidgets\Widgets\Provider\GenericBarChartDataProvider;
+use PHPUnit\Framework\MockObject\Stub;
+use PHPUnit\Framework\TestCase;
+use TYPO3\CMS\Core\Localization\LanguageService;
+
+class GenericBarChartDataProviderTest extends TestCase
+{
+    /**
+     * @var Stub|RepositoryInterface
+     */
+    private $repositoryStub;
+
+    /**
+     * @var Stub|LanguageService
+     */
+    private $languageServiceStub;
+
+    protected function setUp(): void
+    {
+        $this->repositoryStub = $this->createStub(RepositoryInterface::class);
+        $this->languageServiceStub = $this->createStub(LanguageService::class);
+    }
+
+    /**
+     * @test
+     */
+    public function getChartData()
+    {
+        $method = 'some.method';
+        $barColour = '#abc';
+        $barLabel = 'some bar label';
+        $parameters = [
+            'foo' => 'bar',
+            'qux' => 'quu',
+        ];
+
+        $this->repositoryStub
+            ->method('find')
+            ->with($method, new ParameterBag($parameters))
+            ->willReturn([
+                '2020-07-16' => 1234,
+                '2020-07-15' => 543,
+                '2020-07-14' => 6191,
+            ]);
+
+        $this->languageServiceStub
+            ->method('sL')
+            ->with($barLabel)
+            ->willReturn('another bar label');
+
+        $actual = (new GenericBarChartDataProvider(
+            $this->repositoryStub,
+            $this->languageServiceStub,
+            $method,
+            $barLabel,
+            $barColour,
+            $parameters
+        )
+        )
+            ->getChartData();
+
+        $expected = [
+            'labels' => ['2020-07-16', '2020-07-15', '2020-07-14'],
+            'datasets' => [
+                [
+                    'label' => 'another bar label',
+                    'backgroundColor' => $barColour,
+                    'data' => [1234, 543, 6191],
+                ],
+            ],
+        ];
+
+        self::assertSame($expected, $actual);
+    }
+}
