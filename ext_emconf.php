3f62b5b27e27c710450e53e1644d744a5e51d1ad
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-04-13 17:27:05 +0200

    [TASK] Add dashboard as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,6 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.11-11.5.99',
+            'dashboard' => '',
         ],
         'conflicts' => [],
         'suggests' => [],
