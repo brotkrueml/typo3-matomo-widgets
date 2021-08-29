ac7ba8cfb5608b55f0caa04fcb8a7e420e6d3ed1
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-08-29 15:54:35 +0200

    [TASK] Remove unnecessary anonymous function

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -7,62 +7,60 @@
  * LICENSE.txt file that was distributed with this source code.
  */
 
-(function () {
-    $GLOBALS['SiteConfiguration']['site']['columns'] += [
-        'matomoWidgetsTitle' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':title',
-            'description' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':title.description',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
-            ],
+$GLOBALS['SiteConfiguration']['site']['columns'] += [
+    'matomoWidgetsTitle' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':title',
+        'description' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':title.description',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
         ],
-        'matomoWidgetsUrl' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':url',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
-            ],
+    ],
+    'matomoWidgetsUrl' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':url',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
         ],
-        'matomoWidgetsIdSite' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':idSite',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'int',
-            ],
+    ],
+    'matomoWidgetsIdSite' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':idSite',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'int',
         ],
-        'matomoWidgetsTokenAuth' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':tokenAuth',
-            'config' => [
-                'type' => 'input',
-                'eval' => 'trim',
-            ],
+    ],
+    'matomoWidgetsTokenAuth' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':tokenAuth',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
         ],
-        'matomoWidgetsActiveWidgets' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':activeWidgets',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectCheckBox',
-                'items' => (new Brotkrueml\MatomoWidgets\Configuration\WidgetsProvider())->getItemsForTca(),
-            ],
-        ]
-    ];
+    ],
+    'matomoWidgetsActiveWidgets' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':activeWidgets',
+        'config' => [
+            'type' => 'select',
+            'renderType' => 'selectCheckBox',
+            'items' => (new Brotkrueml\MatomoWidgets\Configuration\WidgetsProvider())->getItemsForTca(),
+        ],
+    ]
+];
 
-    $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',
-        --div--;' . Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':matomoWidgets,
-        matomoWidgetsTitle,
-        --palette--;;matomoWidgetsInstallation,
-        --palette--;;matomoWidgetsActiveWidgets,
-    ';
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',
+    --div--;' . Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':matomoWidgets,
+    matomoWidgetsTitle,
+    --palette--;;matomoWidgetsInstallation,
+    --palette--;;matomoWidgetsActiveWidgets,
+';
 
-    $GLOBALS['SiteConfiguration']['site']['palettes'] += [
-        'matomoWidgetsInstallation' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':matomoInstallation',
-            'showitem' => 'matomoWidgetsUrl, matomoWidgetsIdSite, matomoWidgetsTokenAuth',
-        ],
-        'matomoWidgetsActiveWidgets' => [
-            'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':dashboardWidgets',
-            'showitem' => 'matomoWidgetsActiveWidgets',
-        ],
-    ];
-})();
+$GLOBALS['SiteConfiguration']['site']['palettes'] += [
+    'matomoWidgetsInstallation' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':matomoInstallation',
+        'showitem' => 'matomoWidgetsUrl, matomoWidgetsIdSite, matomoWidgetsTokenAuth',
+    ],
+    'matomoWidgetsActiveWidgets' => [
+        'label' => Brotkrueml\MatomoWidgets\Extension::LANGUAGE_PATH_SITECONF . ':dashboardWidgets',
+        'showitem' => 'matomoWidgetsActiveWidgets',
+    ],
+];
