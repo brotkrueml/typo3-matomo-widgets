4755a137b556a349b39a0f44ba62e24795dfe2bb
Chris Müller <coding@brotkrueml.dev>
2026-09-20 19:14:26 +0200

    Avoid deprecated ecs configuration format

--- ecs.docs.php
+++ ecs.docs.php
@@ -4,10 +4,11 @@ declare (strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $config): void {
-    $config->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
-
-    $config->paths([
+return ECSConfig::configure()
+    ->withSets([
+        __DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php',
+    ])
+    ->withParallel()
+    ->withPaths([
         __DIR__ . '/Documentation',
     ]);
-};
